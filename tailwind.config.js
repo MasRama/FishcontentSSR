57087f17399291e230101a6b54568751f3fa20c9
Bqss <basofi.cucokmeong12@gmail.com>
2022-09-13 22:41:32 +0700

    editing style css and add line clamp plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,70 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './resources/**/*.{js,ts,edge}'
+    './resources/**/**/*.{js,ts,edge}'
   ],
   darkMode : "class",
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    theme: {
+        fontFamily: {
+            'poppins': 'Poppins , sans-serif'
+        },
+
+        extend: {
+            backgroundImage: {
+                login: "url('./../../public/images/texture.webp') "
+            },
+
+            backgroundPosition: {
+                sidebar: 'center '
+            },
+            backgroundSize: {
+                '1.25': '100rem 75rem'
+            }
+
+            ,
+            keyframes: {
+                slidetobottom: {
+                    '0%': {
+                        opacity: 0,
+                        transformOrigin: "top",
+                        transform: "scaleY(0)"
+                    },
+                    "100%": {
+                        opacity: 1,
+                        transformOrigin: "top",
+                        transform: "scaleY(1)"
+                    }
+                },
+                fade: {
+                    '0%': {
+                        opacity: 0,
+                    }
+                    ,
+                    '100%': {
+                        opacity: 1
+                    }
+                }
+
+            },
+
+            colors: {
+                'primary': '#ea580c',
+                'secondary': '#0f172a',
+                'accent': '#fff',
+                'neutral-1': '#000000',
+                'hover-1': '#fb923c',
+                'hover-2': '#94a3b8'
+            },
+            animation: {
+                slidetobottom: 'slidetobottom .3s ease-in',
+                fade: 'fade .5s ease-in',
+                'fade-reverse': 'fade .5s ease-in reverse',
+                'slidetobottom-reverse': 'slidetobottom .3s ease-in reverse'
+            },
+            
+        },
+    },
+    plugins: [
+        require('@tailwindcss/line-clamp')
+    ],
 }
