2e6126f01b3855328c6278fa9cb99b5808744504
Bqss <basofi.cucokmeong12@gmail.com>
2022-09-14 15:10:11 +0700

    testing using json

--- app/Controllers/Http/PublicsController.ts
+++ app/Controllers/Http/PublicsController.ts
@@ -1,5 +1,60 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 
+
+const courses = [
+  {
+    "title": "Cara berternak ikan cupang",
+    "desc": "Disini kamu akan belajar cara membudidayakan ikan cupang secara lengkap. Materi disajikan melalui vidio dan juga E-Book pembelajaran. Kamu juga bisa belajar secara syncronous dengan menghubungi tentor yang tersedia.",
+    "img": "./../../images/edukasi/ed1.webp",
+    "link": "/",
+    "alt": "cupang",
+  },
+  {
+    "title": "Cara berternak ikan cupang",
+    "desc": "Disini kamu akan belajar cara membudidayakan ikan cupang secara lengkap. Materi disajikan melalui vidio dan juga E-Book pembelajaran. Kamu juga bisa belajar secara syncronous dengan menghubungi tentor yang tersedia.",
+    "img": "./../../images/edukasi/ed1.webp",
+    "link": "/",
+    "alt": "cupang",
+  },
+  {
+    "title": "Cara berternak ikan cupang",
+    "desc": "Disini kamu akan belajar cara membudidayakan ikan cupang secara lengkap. Materi disajikan melalui vidio dan juga E-Book pembelajaran. Kamu juga bisa belajar secara syncronous dengan menghubungi tentor yang tersedia.",
+    "img": "./../../images/edukasi/ed1.webp",
+    "link": "/",
+    "alt": "cupang",
+  },
+  {
+    "title": "Cara berternak ikan cupang",
+    "desc": "Disini kamu akan belajar cara membudidayakan ikan cupang secara lengkap. Materi disajikan melalui vidio dan juga E-Book pembelajaran. Kamu juga bisa belajar secara syncronous dengan menghubungi tentor yang tersedia.",
+    "img": "./../../images/edukasi/ed1.webp",
+    "link": "/",
+    "alt": "cupang",
+  },
+  {
+    "title": "Cara berternak ikan cupang",
+    "desc": "Disini kamu akan belajar cara membudidayakan ikan cupang secara lengkap. Materi disajikan melalui vidio dan juga E-Book pembelajaran. Kamu juga bisa belajar secara syncronous dengan menghubungi tentor yang tersedia.",
+    "img": "./../../images/edukasi/ed1.webp",
+    "link": "/",
+    "alt": "cupang",
+  },
+  {
+    "title": "Cara berternak ikan cupang",
+    "desc": "Disini kamu akan belajar cara membudidayakan ikan cupang secara lengkap. Materi disajikan melalui vidio dan juga E-Book pembelajaran. Kamu juga bisa belajar secara syncronous dengan menghubungi tentor yang tersedia.",
+    "img": "./../../images/edukasi/ed1.webp",
+    "link": "/",
+    "alt": "cupang",
+  },
+  {
+    "title": "Cara berternak ikan cupang",
+    "desc": "Disini kamu akan belajar cara membudidayakan ikan cupang secara lengkap. Materi disajikan melalui vidio dan juga E-Book pembelajaran. Kamu juga bisa belajar secara syncronous dengan menghubungi tentor yang tersedia.",
+    "img": "./../../images/edukasi/ed1.webp",
+    "link": "/",
+    "alt": "cupang",
+  }
+  
+];
+const coursesPerPage = 5;
+
 export default class PublicsController {
   public async home({ view, auth }: HttpContextContract) {
     if (auth.user) {
@@ -8,8 +63,18 @@ export default class PublicsController {
     return view.render("home", { isLogin: "no" });
   }
 
-  public async edu({ view }: HttpContextContract) {
-    return view.render("edukasi");
+  public async edu({ params,view }: HttpContextContract) {
+      return view.render("edukasi", {
+        courses: courses.filter((v, i) => {
+          if (
+            i >= (params.id - 1) * coursesPerPage &&
+            i <= params.id * coursesPerPage
+          ) {
+            return v;
+          }
+        }),
+        pagination: Array(Math.ceil(courses.length / coursesPerPage)).fill("")
+      });
   }
 
   public async sharing({ view }: HttpContextContract) {
