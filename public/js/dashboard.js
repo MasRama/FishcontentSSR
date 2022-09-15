af7f049435049e74df7207f79afb0d9bc22b9ef4
Bqss <basofi.cucokmeong12@gmail.com>
2022-09-15 19:53:16 +0700

    fix: dasboard sidebar and UI

---
+++ public/js/dashboard.js
@@ -0,0 +1,6 @@
+const sidebar = document.querySelector('.sidebar');
+const sidebarToggler = document.querySelector('.sidebar__toggler');
+console.log("udin");
+sidebarToggler.onclick = () => {
+    console.log('ss')
+}
